3f08b63af976d5da7de6031fc0960d11f34cb48a
s0n1k <team@litecoinz.info>
2018-06-21 23:19:04 +0200

    Reset the difficulty as asked from pools and miners

--- src/version.h
+++ src/version.h
@@ -10,13 +10,13 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 170003;
+static const int PROTOCOL_VERSION = 170004;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! Disconnect from peers older than this protocol version.
-static const int MIN_PEER_PROTO_VERSION = 170003;
+static const int MIN_PEER_PROTO_VERSION = 170004;
 
 //! nTime field added to CAddress, starting with this version;
 //! the older encoding that omits nTime is only used in "version" messages.
