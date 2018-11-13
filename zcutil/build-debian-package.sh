035bef425e584a2d901c621f55b1e87e006ba529
LitecoinZ <team@litecoinz.info>
2018-11-13 12:34:21 +0100

    Remove deprecated 'litecoinz-addrgen'

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -41,7 +41,6 @@ chmod 0755 -R $BUILD_DIR/*
 cp $SRC_PATH/src/litecoinzd $DEB_BIN
 cp $SRC_PATH/src/litecoinz-cli $DEB_BIN
 cp $SRC_PATH/src/litecoinz-tx $DEB_BIN
-cp $SRC_PATH/src/litecoinz-addrgen $DEB_BIN
 cp $SRC_PATH/src/qt/litecoinz-qt $DEB_BIN
 cp $SRC_PATH/zcutil/fetch-params.sh $DEB_BIN/litecoinz-fetch-params
 # Copy docs
@@ -71,8 +70,8 @@ gzip --best -n $DEB_MAN/litecoinz-fetch-params.1
 cd $SRC_PATH/contrib
 
 # Create the control file
-strip $DEB_BIN/litecoinzd $DEB_BIN/litecoinz-cli $DEB_BIN/litecoinz-tx $DEB_BIN/litecoinz-addrgen $DEB_BIN/litecoinz-qt
-dpkg-shlibdeps $DEB_BIN/litecoinzd $DEB_BIN/litecoinz-cli $DEB_BIN/litecoinz-tx $DEB_BIN/litecoinz-addrgen $DEB_BIN/litecoinz-qt
+strip $DEB_BIN/litecoinzd $DEB_BIN/litecoinz-cli $DEB_BIN/litecoinz-tx $DEB_BIN/litecoinz-qt
+dpkg-shlibdeps $DEB_BIN/litecoinzd $DEB_BIN/litecoinz-cli $DEB_BIN/litecoinz-tx $DEB_BIN/litecoinz-qt
 dpkg-gencontrol -P$BUILD_DIR -v$DEBVERSION
 
 # Create the Debian package
