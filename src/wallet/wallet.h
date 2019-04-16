01f1625042ae75bea11039dfd72b3eb6f1d3dddc
LitecoinZ <team@litecoinz.info>
2019-04-16 17:01:09 +0200

    Move CTransAction::IsEquivalentTo to CWalletTx

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -584,6 +584,9 @@ public:
         return (GetDebit(filter) > 0);
     }
 
+    // True if only scriptSigs are different
+    bool IsEquivalentTo(const CWalletTx& tx) const;
+
     bool InMempool() const;
     bool IsTrusted() const;
 
