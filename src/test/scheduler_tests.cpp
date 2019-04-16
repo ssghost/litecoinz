8cea9d8b5e7c9e6d857771d51631692435a3548f
LitecoinZ <team@litecoinz.info>
2019-04-16 18:07:26 +0200

    [test] Disable scheduler test manythreads

--- src/test/scheduler_tests.cpp
+++ src/test/scheduler_tests.cpp
@@ -33,6 +33,7 @@ static void MicroSleep(uint64_t n)
     boost::this_thread::sleep_for(boost::chrono::microseconds(n));
 }
 
+#if 0 /* Disabled for now because there is a race condition issue in this test - see #6540 */
 BOOST_AUTO_TEST_CASE(manythreads)
 {
     seed_insecure_rand(false);
@@ -108,5 +109,6 @@ BOOST_AUTO_TEST_CASE(manythreads)
     }
     BOOST_CHECK_EQUAL(counterSum, 200);
 }
+#endif
 
 BOOST_AUTO_TEST_SUITE_END()
