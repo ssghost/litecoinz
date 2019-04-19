e7c3ad23d459f675da7078d928f8dc51994e1d61
LitecoinZ <team@litecoinz.info>
2019-04-19 16:31:06 +0200

    Avoid std::locale/imbue in DateTimeStrFormat

--- src/utiltime.cpp
+++ src/utiltime.cpp
@@ -10,9 +10,10 @@
 #include "utiltime.h"
 
 #include <atomic>
-
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread.hpp>
+#include <ctime>
+#include <tinyformat.h>
 
 using namespace std;
 
@@ -77,24 +78,23 @@ void MilliSleep(int64_t n)
 #endif
 }
 
-std::string DateTimeStrFormat(const char* pszFormat, int64_t nTime)
-{
-    // std::locale takes ownership of the pointer
-    std::locale loc(std::locale::classic(), new boost::posix_time::time_facet(pszFormat));
-    std::stringstream ss;
-    ss.imbue(loc);
-    ss << boost::posix_time::from_time_t(nTime);
-    return ss.str();
-}
-
 std::string FormatISO8601DateTime(int64_t nTime) {
-    return DateTimeStrFormat("%Y-%m-%dT%H:%M:%SZ", nTime);
+    struct tm ts;
+    time_t time_val = nTime;
+    gmtime_r(&time_val, &ts);
+    return strprintf("%04i-%02i-%02iT%02i:%02i:%02iZ", ts.tm_year + 1900, ts.tm_mon + 1, ts.tm_mday, ts.tm_hour, ts.tm_min, ts.tm_sec);
 }
 
 std::string FormatISO8601Date(int64_t nTime) {
-    return DateTimeStrFormat("%Y-%m-%d", nTime);
+    struct tm ts;
+    time_t time_val = nTime;
+    gmtime_r(&time_val, &ts);
+    return strprintf("%04i-%02i-%02i", ts.tm_year + 1900, ts.tm_mon + 1, ts.tm_mday);
 }
 
 std::string FormatISO8601Time(int64_t nTime) {
-    return DateTimeStrFormat("%H:%M:%SZ", nTime);
+    struct tm ts;
+    time_t time_val = nTime;
+    gmtime_r(&time_val, &ts);
+    return strprintf("%02i:%02i:%02iZ", ts.tm_hour, ts.tm_min, ts.tm_sec);
 }
