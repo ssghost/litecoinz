ccbfeb4a33ff523d986b28fe99ac79eb5f9b11c0
LitecoinZ <team@litecoinz.info>
2019-04-22 20:34:04 +0200

    Fix a type error that would not compile on Apple LLVM

--- src/httpserver.h
+++ src/httpserver.h
@@ -37,7 +37,7 @@ void StopHTTPServer();
 bool UpdateHTTPServerLogging(bool enable);
 
 /** Handler for requests to a certain HTTP path */
-typedef std::function<void(HTTPRequest* req, const std::string &)> HTTPRequestHandler;
+typedef std::function<bool(HTTPRequest* req, const std::string &)> HTTPRequestHandler;
 /** Register handler for prefix.
  * If multiple handlers match a prefix, the first-registered one will
  * be invoked.
