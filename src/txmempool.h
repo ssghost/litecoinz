6ecec3d100baed055b76b9aba49338a240324ec5
LitecoinZ <team@litecoinz.info>
2019-05-01 15:57:35 +0200

    Replace mapNextTx with slimmer setSpends

--- src/txmempool.h
+++ src/txmempool.h
@@ -13,6 +13,7 @@
 
 #include <amount.h>
 #include <coins.h>
+#include <indirectmap.h>
 #include <primitives/transaction.h>
 #include <sync.h>
 
@@ -330,20 +331,6 @@ struct TxMempoolInfo
     CFeeRate feeRate;
 };
 
-/** An inpoint - a combination of a transaction and an index n into its vin */
-class CInPoint
-{
-public:
-    const CTransaction* ptx;
-    uint32_t n;
-
-    CInPoint() { SetNull(); }
-    CInPoint(const CTransaction* ptxIn, uint32_t nIn) { ptx = ptxIn; n = nIn; }
-    void SetNull() { ptx = nullptr; n = (uint32_t) -1; }
-    bool IsNull() const { return (ptx == nullptr && n == (uint32_t) -1); }
-    size_t DynamicMemoryUsage() const { return 0; }
-};
-
 /**
  * CTxMemPool stores valid-according-to-the-current-best-chain
  * transactions that may be included in the next block.
@@ -510,7 +497,7 @@ private:
     std::vector<indexed_transaction_set::const_iterator> GetSortedDepthAndScore() const;
 
 public:
-    std::map<COutPoint, CInPoint> mapNextTx;
+    indirectmap<COutPoint, const CTransaction*> mapNextTx;
     std::map<uint256, std::pair<double, CAmount> > mapDeltas;
 
     /** Create a new CTxMemPool.
