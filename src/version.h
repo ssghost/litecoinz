ceebe7838ae793252900e1228d18ca611c453263
LitecoinZ <team@litecoinz.info>
2019-05-05 20:29:18 +0200

    Update 'src/version.h'

--- src/version.h
+++ src/version.h
@@ -15,15 +15,21 @@ static const int PROTOCOL_VERSION = 170015;
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
-//! Disconnect from peers older than this protocol version.
-static const int MIN_PEER_PROTO_VERSION = 170004;
+//! In this version, 'getheaders' was introduced.
+static const int GETHEADERS_VERSION = 170004;
+
+//! disconnect from peers older than this proto version
+static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
 
 //! nTime field added to CAddress, starting with this version;
-//! the older encoding that omits nTime is only used in "version" messages.
+//! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
+
+//! "mempool" command, enhanced "getdata" behavior starts with this version
+static const int MEMPOOL_GD_VERSION = 60002;
 
 //! "filter*" commands are disabled without NODE_BLOOM after and including this version
 static const int NO_BLOOM_VERSION = 170004;
